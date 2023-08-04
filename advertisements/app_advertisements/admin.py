ce0073cb6c86319eae466a7fa3ba10ba984dd5c9
Nick <122867236+Nikolay1562005@users.noreply.github.com>
2023-08-04 13:40:38 +0700

    I have castomized the admin panel

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'auction', 'created_date', 'updated_date']
+    list_filter = ['price', 'auction', 'create_at']
+    actions = ['make_auction_as_true', 'make_auction_as_false']
+    fieldsets = (
+        ('Общие', {
+            'fields': ('title', 'description')
+        }),
+        ('Финансы', {
+            'fields': ('price', 'auction'),
+            'classes': ['collapse']
+        })
+    )
+
+    @admin.action(description='добавить возможность торга')
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+
+    @admin.action(description='убрать возможность торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils import timezone
 
 
 class Advertisement(models.Model):
@@ -7,8 +10,28 @@ class Advertisement(models.Model):
     description = models.TextField('Описание')
     price = models.DecimalField("Цена", max_digits=10, decimal_places=2)  # max_digits - цифр в числе (все) decimal_places - кол-во цифр после запятой
     auction = models.BooleanField("Торг", help_text="Укажите возможен ли торг")
-    create_at = models.DateTimeField(auto_now_add=True)
+    create_at = models.DateTimeField("Время создания", auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        if self.create_at.date() == timezone.now().date():
+            created_time = self.create_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>'.format(created_time)
+            )
+        return self.create_at.strftime('%d.%m.%Y')
+
+    @admin.display(description="Дата изменения")
+    def updated_date(self):
+        if self.update_at.date() == timezone.now().date():
+            update_time = self.update_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: violet; font-weight: bold;">Сегодня в {}</span>'.format(update_time)
+            )
+        return self.update_at.strftime('%d.%m.%Y')
+
+
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price = {self.price})"
